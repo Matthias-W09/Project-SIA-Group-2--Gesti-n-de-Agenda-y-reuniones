848e4207177230ba8a1fcd751d681dd9cb4dc8c8
Matthias-W09 <155279145+Matthias-W09@users.noreply.github.com>
2024-04-24 22:04:05 -0400

    Update Reunion.java, se agregaron las clases fecha y hora de la librería java. Time

--- SIA Final/src/main/java/Models/Reunion.java
+++ SIA Final/src/main/java/Models/Reunion.java
@@ -1,21 +1,31 @@
 package Models;
 
-import java.text.DecimalFormat;
+//import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Reunion {
 // Atributos de Reunion
 private String nombre;
 private String lugar;
-private String fecha;
-private double hora;
+private LocalDate fecha;
+private LocalTime hora;
+// private String fecha;
+// private double hora;
 
 // Constructor de Reunion
-public Reunion(String nombre, String lugar, String fecha, double hora) {
-    this.nombre = nombre;
-    this.lugar = lugar;
-    this.fecha = fecha;
-    this.hora = hora;
-}
+public Reunion(String nombre, String lugar, LocalDate fecha, LocalTime hora) {
+      this.nombre = nombre;
+      this.lugar = lugar;
+      this.fecha = fecha;
+      this.hora = hora;
+  }
+//public Reunion(String nombre, String lugar, String fecha, double hora) {
+//    this.nombre = nombre;
+//    this.lugar = lugar;
+//    this.fecha = fecha;
+//    this.hora = hora;
+//}
 
 // Getters y Setters de Reunion
 public String getNombre() {
@@ -34,19 +44,32 @@ public void setLugar(String lugar) {
     this.lugar = lugar;
 }
 
-public String getFecha() {
+public LocalDate getFecha() {
     return fecha;
 }
+// public String getFecha() {
+//    return fecha;
+// }
 
-public void setFecha(String fecha) {
+public void setFecha(LocalDate fecha) {
     this.fecha = fecha;
 }
+  
+// public void setFecha(String fecha) {
+//    this.fecha = fecha;
+// }
 
-public double getHora() {
+public LocalTime getHora() {
     return hora;
 }
+// public double getHora() {
+//    return hora;
+// }
 
-public void setHora(double hora) {
+public void setHora(LocalTime hora) {
     this.hora = hora;
 }
+// public void setHora(double hora) {
+//    this.hora = hora;
+// }
 } 
