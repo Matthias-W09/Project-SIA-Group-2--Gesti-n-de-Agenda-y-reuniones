837448514585925c6d1083140abd87bb1997f5a3
Matthias-W09 <155279145+Matthias-W09@users.noreply.github.com>
2024-04-24 22:02:01 -0400

    actualizacion de archivo controller.java, se agrego las clases de fecha y hora de  la libreria java.time

--- SIA Final/src/main/java/Controller/Controller.java
+++ SIA Final/src/main/java/Controller/Controller.java
@@ -6,6 +6,8 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
+import java.time.LocalDate; // new
+import java.time.LocalTime; // new
 import Models.Persona;
 import Models.Reunion;
 
@@ -25,39 +27,55 @@ public class Controller implements MouseListener {
       personas.add(persona1);
       personas.add(persona2);
       // Creación de algunas reuniones y agregación a la lista de reuniones de la persona1 y persona2
-      Reunion reunion1 = new Reunion("Reunion Ventas","Sala de Conferencias", "2024-03-20", 10);
-      Reunion reunion2 = new Reunion("Reunion Jefe","Oficina", "2024-03-22", 15.5);
-      persona1.agregarReuniones(reunion1);
-      persona1.agregarReuniones(reunion2);
-      persona2.agregarReuniones(reunion1);
-      persona2.agregarReuniones(reunion2);
+      Reunion reunion1 = new Reunion("Reunion Ventas", "Sala de Conferencias", LocalDate.of(2024, 3, 20),
+        LocalTime.of(10, 0));
+     // Reunion reunion1 = new Reunion("Reunion Ventas","Sala de Conferencias", "2024-03-20", 10);
+
+      Reunion reunion2 = new Reunion("Reunion Jefe", "Oficina", LocalDate.of(2024, 3, 22), LocalTime.of(15, 30));
+
+     // Reunion reunion2 = new Reunion("Reunion Jefe","Oficina", "2024-03-22", 15.5);
+      agregarReuniones(persona1, reunion1);
+      agregarReuniones(persona1, reunion1);
+      agregarReuniones(persona2, reunion1);
+      agregarReuniones(persona2, reunion2);
       //PREGUNTAR PROFE SI METODO AGREGAR REUNION DEBE IR EN CLASE PERSONA O EN CLASE CONTROLLER
   }
 
   public void agregarPersona(String nombre, String email){
+    for(Persona persona : personas){
+      if(persona.getNombre().equals(nombre)){
+        //Mostrar que ya existe por pantalla
+        return;
+      }
+    }
     personas.add(new Persona(nombre, email));
     return;
   }
 
-  public void agregarReuniones(String nombrePersona, String nombre, String lugar, String fecha, double hora){
-    Reunion newReunion = new Reunion(nombre, lugar, fecha, hora);
+  public void agregarReuniones(Persona persona,Reunion newReunion){
     for (int i = 0; i < personas.size(); i++) {
-        if(personas.get(i).getNombre() == nombrePersona){
-          reuniones.put(newReunion.getNombre(), newReunion);
-          return;
+      if (personas.get(i).getNombre().equals(persona.getNombre())){
+        for(int j = 0; j < personas.get(i).getReuniones().size(); j++){
+          if(personas.get(i).getReuniones().get(newReunion.getNombre())!=null){
+            //Mostrar que ya existe por pantalla
+            return;
+          }
         }
+        persona.getReuniones().put(newReunion.getNombre(), newReunion);
+        return;
+      }
     }
-    //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
     return;
+    //Poner por Dialogo no se encontro persona
   }
 
   public void mostrarReuniones(String nombrePersona){
     for (int i = 0; i < personas.size(); i++) {
-        if(personas.get(i).getNombre() == nombrePersona){
+        if (personas.get(i).getNombre().equals(nombrePersona)){
           //PONER DATOS DE LA PERSONA EN VENTANA
           return;
         }
-      //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+      //PONER WEA EN dialogo DE QUE NO SE ENCONTRO LA PERSONA
       return;
     }
   }
@@ -70,7 +88,7 @@ public class Controller implements MouseListener {
 
   public void eliminarReunion(String nombrePersona){
     for (int i = 0; i < personas.size(); i++) {
-        if(personas.get(i).getNombre() == nombrePersona){
+        if(personas.get(i).getNombre().equals(nombrePersona)){
           //mostrar reuniones de la peronsa
           //preguntar nombre de la reunion a eliminar
           //j el valor que entregue el usuario a eleiminar(numero o nombre nose)
@@ -82,44 +100,88 @@ public class Controller implements MouseListener {
 
   public void eliminarPersona(String nombrePersona){
     for (int i = 0; i < personas.size(); i++) {
-        if(personas.get(i).getNombre() == nombrePersona){
+        if(personas.get(i).getNombre().equals(nombrePersona)){
           personas.remove(i);
           return;
         }
     }
-    //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+    //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
     return;
   }
 
-  //POSIBLE SOBRECARGA DE METODOS CON PARAMETROS
-  public void editarReunion(String nombrePersona, String nombreReunion, String lugar, String fecha, double hora){
+  //POSIBLE SOBRECARGA DE METODOS CON PARAMETROS --->> new 
+  public void editarReunion(String nombrePersona, String nombreReunion, String nuevoNombreReunion, String lugar, LocalDate fecha, LocalTime hora) {
+      for (int i = 0; i < personas.size(); i++) {
+          if (personas.get(i).getNombre().equals(nombrePersona)) {
+              if (personas.get(i).getReuniones().get(nombreReunion) != null) {
+                  personas.get(i).getReuniones().get(nombreReunion).setNombre(nuevoNombreReunion);
+                  personas.get(i).getReuniones().get(nombreReunion).setLugar(lugar);
+                  personas.get(i).getReuniones().get(nombreReunion).setFecha(fecha);
+                  personas.get(i).getReuniones().get(nombreReunion).setHora(hora);
+                  return;
+              }
+              // PONER EN VENTANA DE QUE NO SE ENCONTRO LA REUNION
+          }
+      }
+    //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+  }
+
+  //Sobrecarga sin fecha ni hora
+  public void editarReunion(String nombrePersona, String nombreReunion, String nuevoNombreReunion, String lugar,) {
+      for (int i = 0; i < personas.size(); i++) {
+          if (personas.get(i).getNombre().equals(nombrePersona)) {
+              if (personas.get(i).getReuniones().get(nombreReunion) != null) {
+                  personas.get(i).getReuniones().get(nombreReunion).setNombre(nuevoNombreReunion);
+                  personas.get(i).getReuniones().get(nombreReunion).setLugar(lugar);
+                  return;
+              }
+              // PONER EN VENTANA DE QUE NO SE ENCONTRO LA REUNION
+          }
+      }
+    //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+  }
+
+  //sobrecarga solo fecha
+  public void editarReunion(String nombrePersona, String nombreReunion, LocalDate fecha, LocalTime hora) {
+      for (int i = 0; i < personas.size(); i++) {
+          if (personas.get(i).getNombre().equals(nombrePersona)) {
+              if (personas.get(i).getReuniones().get(nombreReunion) != null) {
+                  personas.get(i).getReuniones().get(nombreReunion).setFecha(fecha);
+                  personas.get(i).getReuniones().get(nombreReunion).setHora(hora);
+                  return;
+              }
+              // PONER EN VENTANA DE QUE NO SE ENCONTRO LA REUNION
+          }
+      }
+    //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+  }
+  // original
+ /*public void editarReunion(String nombrePersona, String nombreReunion, String lugar, String fecha, double hora){
     for (int i = 0; i < personas.size(); i++){
       if(personas.get(i).getNombre() == nombrePersona){
-        for (int j = 0; j < personas.get(i).getReuniones().size(); j++){
-          if(personas.get(i).getReuniones().get(j).getNombre() == nombreReunion){
-            personas.get(i).getReuniones().get(j).setLugar(lugar);
-            personas.get(i).getReuniones().get(j).setFecha(fecha);
-            personas.get(i).getReuniones().get(j).setHora(hora);
-            return;
+        if(personas.get(i).getReuniones().get(nombreReunion)!=null){
+              personas.get(i).getReuniones().get(nombreReunion).setNombre(nombreReunion);
+              personas.get(i).getReuniones().get(nombreReunion).setLugar(lugar);
+              personas.get(i).getReuniones().get(nombreReunion).setFecha(fecha);
+              personas.get(i).getReuniones().get(nombreReunion).setHora(hora);
+              return;
             }
-          //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA REUNION  
-          return;
+          //PONER EN VENTANA DE QUE NO SE ENCONTRO LA REUNION  
           }
-    //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
-      return;
-      }
+    }
   }
-}
+  //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+}*/
 
   public void editarPersona(String nombrePersona, String nombre, String email){
     for (int i = 0; i < personas.size(); i++){
-      if(personas.get(i).getNombre() == nombrePersona){
+      if(personas.get(i).getNombre().equals(nombrePersona)){
         personas.get(i).setNombre(nombre);
         personas.get(i).setEmail(email);
         return;
       }
     }
-    //PONER WEA EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
+    //PONER EN VENTANA DE QUE NO SE ENCONTRO LA PERSONA
     return;
   }
 
